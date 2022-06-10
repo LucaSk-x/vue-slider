49ebc3fd2b398aac46241f1b3946605262c85d55
LucaSk-x <lucapiccinno64@gmail.com>
2022-06-10 15:04:55 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const slides = [
+    {
+        image: 'img/01.jpg',
+        title: 'Svezia',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        image: 'img/02.jpg',
+        title: 'Svizzera',
+        text: 'Lorem ipsum.',
+    },
+    {
+        image: 'img/03.jpg',
+        title: 'Gran Bretagna',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        image: 'img/04.jpg',
+        title: 'Germania',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+    },
+    {
+        image: 'img/05.jpg',
+        title: 'Paradise',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+    }
+];
+
+console.log(slides);
+
+let app = new Vue (
+    {
+        el: '#app',
+
+        data: {
+            list: slides,
+            source: slides[4].image,
+        },
+
+        methods: {
+            increment: function (i) {
+                slides[i]
+                i++
+            },
+
+            decrement: function (i) {
+                slides[i] = this.source 
+                i--
+            }
+        },
+    }
+)
